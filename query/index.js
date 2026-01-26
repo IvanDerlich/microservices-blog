0d2bb74accc7b1a8ca3e21fa0d643e17957cfce9
Ivan Derlich <a@ivanderlich.com>
2026-01-26 17:57:18 -0300

    Handle post events in query service

--- query/index.js
+++ query/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const posts = {};
+
 const app = express();
 app.use(bodyParser.json());
 app.use(
@@ -17,6 +19,32 @@ app.get("/events", (req, res) => {
 app.post("/events", async (req, res) => {
   const event = req.body;
   console.log("Received Event:", event);
+  switch (event.type) {
+    case "PostCreated": {
+      const { id, title } = event.data;
+      posts[id] = { id, title, comments: [] };
+      break;
+    }
+    case "CommentCreated": {
+      const { id, content, postId } = event.data;
+      const post = posts[postId];
+      if (post) {
+        console.log("Adding comment to post:", postId);
+        post.comments.push({ id, content });
+      } else {
+        console.error("Post not found for CommentCreated event:", postId);
+        res.status(400).send({
+          error: "Post not found for CommentCreated event: " + postId,
+        });
+        return;
+      }
+      break;
+    }
+    default:
+      res.status(400).send({ error: "Unknown event type: " + event.type });
+      return;
+  }
+  console.log("Updated Posts:", posts);
 
   res.send({ status: "OK" });
 });
