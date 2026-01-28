a0f914a1ea6dfae273154007f2bd9f79248260b0
Ivan Derlich <a@ivanderlich.com>
2026-01-28 11:30:23 -0300

    Add missing events handler

--- query/index.js
+++ query/index.js
@@ -85,6 +85,17 @@ app.post("/events", async (req, res) => {
   res.send({ status: "OK" });
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Query service listening on port 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
